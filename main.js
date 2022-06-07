643f5b1ab1a0917d64a46ff6412ed709104d0056
SkyDogzz <thomas.stephan@live.fr>
2022-06-07 15:10:58 +0000

    .

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let aboutContainer = document.getElementById('about-container');
 let progressBars = document.getElementsByClassName('progress-bar');
 let smoothScrollContainer = document.getElementById('smooth-scroll-container');
 let aboutBase = document.querySelectorAll('.about-base');
+let h1Shadow = document.querySelector('#h1-shadow');
+let h1 = document.querySelector('h1');
 
 smoothScrollContainer.addEventListener('scroll', function () {
     const scrollTop = smoothScrollContainer.scrollTop;
@@ -23,12 +25,24 @@ smoothScrollContainer.addEventListener('scroll', function () {
 
     for (let i = 0; i < progressBars.length; i++) {
         if (scrollTop > (scrollTop + topElementToTopViewPortProgressBars[i] - clientHeight) && scrollTop < (scrollTop + topElementToTopViewPortProgressBars[i])) {
-            progressBars[i].children[0].style.width = progressBars[i].children[0].getAttribute('data-percentage') + '%';
+            setTimeout(function () {
+                progressBars[i].children[0].style.width = progressBars[i].children[0].getAttribute('data-percentage') + '%';
+            }, 50 * i)
         }
         else {
             progressBars[i].children[0].style.width = '0%';
         }
     }
+})
+
+/* event on window when mouse mouving */
+window.addEventListener('mousemove', function (e) {
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    let toMovX = (mouseX - window.innerWidth / 2) / window.innerWidth * 100;
+    let toMovY = (mouseY - window.innerHeight / 2) / window.innerHeight * 100;
+    h1Shadow.style.transform = 'translate(calc(-50% + ' + toMovX + 'px), calc(-50% + ' + toMovY + 'px)) rotate(-20deg)';
+    h1.style.transform = 'translate(calc(-50% + ' + -toMovX + 'px), calc(-50% + ' + -toMovY + 'px))';
 })
 
 history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search)
